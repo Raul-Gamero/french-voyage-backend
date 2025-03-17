8cd2aff3c405b30095f6790c770c30f7743d2559
Raul Gamero <raul.a.gamero@gmail.com>
2025-03-17 20:30:04 +0100

    done with the changes

--- src/server.ts
+++ src/server.ts
@@ -15,5 +15,5 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/courses", courseRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Servidor corriendo en el puerto ${PORT}`));
